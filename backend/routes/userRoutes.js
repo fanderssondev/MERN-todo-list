ac8a729f9f8ffd5e9d1d9674723a183586c79ec7
nanobyter <sotarfredrik@gmail.com>
2023-06-19 22:43:00 +0200

    Finished routes and middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,4 @@
 import express from 'express';
-const router = express.Router();
 import {
   authUser,
   registerUser,
@@ -7,10 +6,16 @@ import {
   getUserProfile,
   updateUserProfile,
 } from '../controller/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
 
 router.post('/', registerUser);
 router.post('/auth', authUser);
 router.post('/logout', logoutUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
